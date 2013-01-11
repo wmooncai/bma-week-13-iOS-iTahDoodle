27da8f1df2e5be7b4c9bb611c562afbacaec9c21
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2013-01-10 19:56:27 -0500

    Initial Commit

---
+++ bma-week-13-iOS-iTahDoodle/WAMSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WAMSAppDelegate.h
+//  bma-week-13-iOS-iTahDoodle
+//
+//  Created by Wally on 1/10/13.
+//  Copyright (c) 2013 WAMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAMSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bma-week-13-iOS-iTahDoodle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bma-week-13-iOS-iTahDoodle
+//
+//  Created by Wally on 1/10/13.
+//  Copyright (c) 2013 WAMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WAMSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WAMSAppDelegate class]));
+    }
+}
